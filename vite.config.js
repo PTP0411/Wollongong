c22e34bd28558691c35b39da45e585640e6247b4
Phong T Pham <104152969+PTP0411@users.noreply.github.com>
2025-04-07 23:10:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-	plugins: [
-		
-	]
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        gallery: path.resolve(__dirname, 'gallery.html'),
+        food: path.resolve(__dirname, 'food.html'),
+        place: path.resolve(__dirname, 'place.html'),
+      }
+    }
+  }
 })
